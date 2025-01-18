a711db880d1eeaf0ace903122ce5ac5fa0792d7a
Mogilicharlasiva <mogilicharlasiva@gmail.com>
2025-01-18 16:00:05 -0500

    Adding templates and bootstrap code

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts_data=[
+    {
+        'author': 'siva',
+        'title': 'Blog post 1',
+        'content': 'This is the post 1',
+        'date_posted': 'Jan 1st, 2025'
+    },
+    {
+        'author': 'msk',
+        'title': 'Blog post 2',
+        'content': 'This is the post 2',
+        'date_posted': 'Jan 1st, 2025'
+    }
+]
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>This is the first view route</h1>")
+    context={
+        'posts': posts_data
+    }
+    return render(request, 'blog/home.html', context)
+def about(request):
+    title={
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html', title)
+    
