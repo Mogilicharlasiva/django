76da3b260b2e7d569b5f40cc2677868a3d06a0ab
Mogilicharlasiva <mogilicharlasiva@gmail.com>
2025-01-18 19:22:44 -0500

    Adding Database and Giving admin access

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts_data=[
-    {
-        'author': 'siva',
-        'title': 'Blog post 1',
-        'content': 'This is the post 1',
-        'date_posted': 'Jan 1st, 2025'
-    },
-    {
-        'author': 'msk',
-        'title': 'Blog post 2',
-        'content': 'This is the post 2',
-        'date_posted': 'Jan 1st, 2025'
-    }
-]
+from .models import Post
 # Create your views here.
 
 def home(request):
     context={
-        'posts': posts_data
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
