6275d2122028bac08acd99a3fc936d5f891cc73d
Mogilicharlasiva <mogilicharlasiva@gmail.com>
2025-01-21 22:13:45 -0500

    Registration, Login and Logout setup

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app.apps.AppConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'  # or 'bootstrap5', depending on your choice
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login-page'
